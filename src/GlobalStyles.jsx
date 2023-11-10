9e3eedbd76f7dc4d77832df87a4b095a0283cd69
IvashkaVIP <ivashka.co@gmail.com>
2023-11-11 00:36:46 +0200

    add GlobalStyles & font

---
+++ src/GlobalStyles.jsx
@@ -0,0 +1,76 @@
+import 'modern-normalize';
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+
+ :root {
+    --light-text-color: #ffffff;
+    --dark-text-color: #121417;
+    --grey-text-color: #8A8A89;
+    --light-grey-text-color: rgba(18,20,23,0.5);
+    --accent-text-color: #3470FF;
+    --decoration-color: rgba(18,20,23,0.1)
+    --background-color: #ffffff;
+    --normal-button-color: #3470FF;
+    --hover-button-color: #0B44CD;
+    
+  }
+
+
+/* &.hidden {
+  position: absolute;
+  white-space: nowrap;
+  width: 1px;
+  height: 1px;
+  overflow: hidden;
+  border: 0;
+  padding: 0;
+  clip: rect(0 0 0 0);
+  clip-path: inset(50%);
+  margin: -1px;
+} */
+
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background-color: var(--background-color);
+  font-family: 'Manrope', sans-serif;
+  font-weight: 400;
+  font-style: normal;
+  color: var(--dark-text-color);
+  width: 100%;
+  height: 100vh;
+  margin: 0;
+}
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p {
+  margin: 0;
+}
+a {
+  text-decoration: none;
+}
+ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+img {
+  display: block;
+  max-width: 100%;
+  height: auto;
+}
+button {
+  cursor: pointer;
+}
+`;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { GlobalStyles } from 'GlobalStyles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/RentalCar">
+      <GlobalStyles />
       <App />
     </BrowserRouter>
   </React.StrictMode>
