b13e87eb2368942f98bb8e4fcdef4d8133971831
IvashkaVIP <ivashka.co@gmail.com>
2023-12-03 22:02:44 +0200

    replace loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,1 +1,22 @@
-export const Loader =()=>(<>Loader</>)
+import { RotatingLines } from 'react-loader-spinner';
+
+export const Loader = ({ display }) => (
+  <div
+    style={{
+    //   display: display,
+            justifyContent: 'center',
+        alignItems: 'center',
+      width: '100%',
+height:'100%',
+    }}
+  >
+    <RotatingLines
+      strokeColor={"grey"}
+      strokeWidth="5"
+      animationDuration="0.75"
+      width="10%"
+      visible={true}
+    />
+  </div>
+);
+
